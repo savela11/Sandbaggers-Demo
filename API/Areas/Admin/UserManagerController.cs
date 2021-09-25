053b3d4cab09e42cea4df9ab5a0705852e11cdba
Andy Savela <savela11@gmail.com>
2021-09-24 23:06:23 -0400

    add user manager controller for admins

---
+++ API/Areas/Admin/UserManagerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interface;
+
+namespace API.Areas.Admin
+{
+    [Area("Admin")]
+    [Authorize(Policy = "Admin")]
+    [ApiController]
+    [Route("api/[area]/[controller]/[action]")]
+    public class UserManagerController : ControllerBase
+    {
+        private readonly IService _service;
+
+        public UserManagerController(IService service)
+        {
+            _service = service;
+        }
+
+
+        public async Task<ActionResult> AllUsers()
+        {
+            var response = await _service.User.Users();
+            if (response.Success == false)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response.Data);
+        }
+    }
+}
